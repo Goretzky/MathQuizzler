bd9211d5ba7ddc7410ea7286645156b8cf7dd9e8
Kern Goretzky <kern.goretzky@gmail.com>
2025-03-28 22:27:50 -0400

    Initial Commit

---
+++ MathQuizzler/MathQuizzlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathQuizzlerApp.swift
+//  MathQuizzler
+//
+//  Created by Kern Goretzky on 2025-03-28.
+//
+
+import SwiftUI
+
+@main
+struct MathQuizzlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MathQuizzlerTests/MathQuizzlerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathQuizzlerTests.swift
+//  MathQuizzlerTests
+//
+//  Created by Kern Goretzky on 2025-03-28.
+//
+
+import Testing
+@testable import MathQuizzler
+
+struct MathQuizzlerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
